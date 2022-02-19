f6ec557490bcb3afc4c810c304519ad3fd508809
imasshaWe <imasha98.we@gmail.com>
2022-02-20 01:47:19 +0530

    set up book module

--- src/main/java/com/debuggers/bookstore/models/BookModel.java
+++ src/main/java/com/debuggers/bookstore/models/BookModel.java
@@ -4,17 +4,21 @@ import com.debuggers.bookstore.repository.ColumnValueMap;
 import com.debuggers.bookstore.repository.DataRepositoryException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class BookModel implements SqlDataModel {
 
     private int id;
     private String name;
     private String description;
+    private String author;
+    private String language;
+    private String publisher;
     private int authorId;
     private int categoryId;
     private int subCategoryId;
-    private int price;
-    private int isbn;
+    private double price;
+    private String isbn;
     private int publisherId;
     private int languageId;
 
@@ -69,7 +73,7 @@ public class BookModel implements SqlDataModel {
         this.subCategoryId = subCategoryId;
     }
 
-    public int getPrice() {
+    public double getPrice() {
         return price;
     }
 
@@ -77,11 +81,11 @@ public class BookModel implements SqlDataModel {
         this.price = price;
     }
 
-    public int getIsbn() {
+    public String getIsbn() {
         return isbn;
     }
 
-    public void setIsbn(int isbn) {
+    public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
@@ -109,13 +113,47 @@ public class BookModel implements SqlDataModel {
         this.isDelete = isDelete;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
     @Override
     public void readSQL(ResultSet resultSet) throws DataRepositoryException {
-
+        try {
+            this.id = resultSet.getInt("id");
+            this.name = resultSet.getString("name");
+            this.author = resultSet.getString("author");
+            this.categoryId = resultSet.getInt("category_id");
+            this.subCategoryId = resultSet.getInt("sub_category_id");
+            this.languageId = resultSet.getInt("language_id");
+            this.language = resultSet.getString("language");
+            this.price = resultSet.getDouble("price");
+            this.isbn = resultSet.getString("isbn");
+            this.publisher = resultSet.getString("publisher");
+            this.isDelete = 0;
+        } catch (SQLException e) {
+            throw new DataRepositoryException(e, e.getMessage());
+        }
     }
 
     @Override
     public ColumnValueMap writeSQL(ColumnValueMap columnValueMap) throws DataRepositoryException {
-        return null;
+        columnValueMap.set("name", this.name);
+        columnValueMap.set("description", this.description);
+        columnValueMap.set("author_id", this.authorId);
+        columnValueMap.set("category_id", this.categoryId);
+        columnValueMap.set("sub_category_id", this.subCategoryId);
+        columnValueMap.set("isbn", this.isbn);
+        columnValueMap.set("slug", this.isbn);
+        columnValueMap.set("is_delete", this.isDelete);
+        return columnValueMap;
     }
 }
--- src/main/java/com/debuggers/bookstore/views/book/Book.java
+++ src/main/java/com/debuggers/bookstore/views/book/Book.java
@@ -8,8 +8,11 @@ import com.debuggers.bookstore.views.PageView;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Arrays;
 import java.util.List;
 
 public class Book extends PageView {
@@ -36,33 +39,65 @@ public class Book extends PageView {
     private List<SqlDataModel> authorList;
     private List<SqlDataModel> publisherList;
     private List<SqlDataModel> languageList;
+    private List<SqlDataModel> categoryList;
     private BookModel bookModel;
+
+    private String fetchCategoriesQuery = """
+            SELECT category,book_category.id,book_category.is_delete,
+            IFNULL(GROUP_CONCAT(sub_category),'N/A') AS 'sub_category',
+            IFNULL(GROUP_CONCAT(book_sub_category.id),'N/A') AS 'sub_category_ids'
+            FROM book_category
+            LEFT JOIN book_sub_category
+            ON book_sub_category.category_id = book_category.id AND book_sub_category.is_delete = 0
+            WHERE book_category.is_delete = 0
+            GROUP BY book_category.id
+            """;
+    private String bookFetchQuery = """
+            SELECT
+            book.id,book.name AS NAME,price,isbn,category_id,sub_category_id,language_id,
+            CONCAT(book_author.fname,' ',book_author.lname) AS author,
+            book_publisher.name AS publisher,
+            book_language.language
+            FROM book
+            INNER JOIN book_author ON book_author.id = book.author_id
+            INNER JOIN book_publisher ON book_publisher.id = book.publisher_id
+            INNER JOIN book_language ON book_language.id = book.language_id
+            WHERE book.is_delete = '0'
+            """;
 
     public Book(DataRepository dataRepository) {
         super();
         this.dataRepository = dataRepository;
-        dataRepository.createStatement("book");
         initComponents();
+    }
+
+    private void initComponents() {
+        add(mainPanel);
+        loadLanguage();
+        loadAuthor();
+        loadPublisher();
+        loadCategory();
+        createTable();
 
         btnClear.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                //clearFields();
+                clearFields();
             }
         });
         btnDelete.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                //delete();
+                delete();
             }
         });
         btnEdit.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                //edit();
+                edit();
             }
         });
         btnSave.addMouseListener(new MouseAdapter() {
@@ -72,15 +107,12 @@ public class Book extends PageView {
                 insert();
             }
         });
-
-        createTable();
-    }
-
-    private void initComponents() {
-        add(mainPanel);
-        loadLanguage();
-        loadAuthor();
-        loadPublisher();
+        comboCategory.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                loadSubCategory();
+            }
+        });
     }
 
     private void loadLanguage() {
@@ -91,14 +123,14 @@ public class Book extends PageView {
 
             languageList = dataRepository.get(BookLanguageModel.class);
             comboLanguage.addItem("Choose");
-            languageList.stream().forEach((e)->{
+            languageList.stream().forEach((e) -> {
                 BookLanguageModel language = (BookLanguageModel) e;
                 comboLanguage.addItem(language.getLanguage());
             });
 
         } catch (DataRepositoryException e) {
 
-            Alert.showError("Database Error:",e.getMessage());
+            Alert.showError("Database Error:", e.getMessage());
 
         }
     }
@@ -106,20 +138,20 @@ public class Book extends PageView {
     private void loadAuthor() {
 
         dataRepository.createStatement("book_author");
-        dataRepository.where("is_delete",0);
+        dataRepository.where("is_delete", 0);
 
         try {
 
             authorList = dataRepository.get(BookAuthorModel.class);
             comboAuthor.addItem("Choose");
-            authorList.stream().forEach((e)->{
+            authorList.stream().forEach((e) -> {
                 BookAuthorModel author = (BookAuthorModel) e;
-                comboAuthor.addItem(author.getFirstName()+" "+author.getLastName());
+                comboAuthor.addItem(author.getFirstName() + " " + author.getLastName());
             });
 
         } catch (DataRepositoryException e) {
 
-            Alert.showError("Database Error:",e.getMessage());
+            Alert.showError("Database Error:", e.getMessage());
 
         }
     }
@@ -127,22 +159,57 @@ public class Book extends PageView {
     private void loadPublisher() {
 
         dataRepository.createStatement("book_publisher");
-        dataRepository.where("is_delete",0);
+        dataRepository.where("is_delete", 0);
 
         try {
 
             publisherList = dataRepository.get(BookPublisherModel.class);
             comboPublisher.addItem("Choose");
-            publisherList.stream().forEach((e)->{
+            publisherList.stream().forEach((e) -> {
                 BookPublisherModel publisher = (BookPublisherModel) e;
                 comboPublisher.addItem(publisher.getName());
             });
 
         } catch (DataRepositoryException e) {
 
-            Alert.showError("Database Error:",e.getMessage());
+            Alert.showError("Database Error:", e.getMessage());
 
         }
+    }
+
+    private void loadCategory() {
+
+        try {
+
+            categoryList = dataRepository.executeQuery(fetchCategoriesQuery, CategoryModel.class);
+            comboCategory.addItem("Choose");
+            categoryList.stream().forEach((e) -> {
+                CategoryModel category = (CategoryModel) e;
+                comboCategory.addItem(category.getCategory());
+            });
+
+        } catch (DataRepositoryException e) {
+
+            Alert.showError("Database Error:", e.getMessage());
+
+        }
+    }
+
+    private void loadSubCategory() {
+        comboSubCategory.removeAllItems();
+        int selectedIndex = comboCategory.getSelectedIndex() - 1;
+        if (selectedIndex < 0) {
+            comboSubCategory.addItem("N/A");
+            return;
+        }
+        List<String> subs = ((CategoryModel) categoryList.get(selectedIndex)).getSubCategoriesList();
+        if (subs.isEmpty()) {
+            comboSubCategory.addItem("N/A");
+        } else {
+            comboSubCategory.addItem("Choose");
+            subs.forEach((e) -> comboSubCategory.addItem(e));
+        }
+
     }
 
     private void insert() {
@@ -173,9 +240,86 @@ public class Book extends PageView {
         }
     }
 
+
+    private void edit() {
+        final int selectedRow = table.getSelectedRow();
+
+        if (selectedRow == -1) {
+            Alert.showError("Edit Error:", "Please select the record that you want to edit!");
+            return;
+        }
+
+        bookModel = (BookModel) dataList.get(selectedRow);
+
+        txtName.setText(bookModel.getName());
+        txtISBN.setText(bookModel.getIsbn());
+        txtPrice.setText(String.valueOf(bookModel.getPrice()));
+        textareaDescription.setText(bookModel.getDescription());
+    }
+
+    private void delete() {
+        final int selectedRow = table.getSelectedRow();
+
+        if (selectedRow == -1) {
+            Alert.showError("Deletion Error:", "Please select the record that you want to delete!");
+            return;
+        }
+
+        final int response = Alert.showConfirm("Delete record", "Are you sure you want to delete?");
+        bookModel = (BookModel) dataList.get(selectedRow);
+        bookModel.setIsDelete(1);
+
+        if (response == 0) {
+
+            try {
+
+                dataRepository.createStatement("book");
+                dataRepository.where("id", bookModel.getId());
+                dataRepository.update(bookModel);
+                bookModel = null;
+                createTable();
+
+            } catch (DataRepositoryException exception) {
+
+                Alert.showError("Deletion Error:", exception.getMessage());
+
+            }
+        }
+
+    }
+
+    private void clearFields() {
+        Arrays.stream(formPanel.getComponents()).forEach((c) -> {
+            if (c instanceof JTextField) {
+                ((JTextField) c).setText(null);
+            }
+        });
+        bookModel = null;
+    }
+
     private void createTable() {
-        final String[] columnNames = {"Name", "ISBN", "Email", "Author", "Publisher", "Language", "Price"};
+        final String[] columnNames = {"Name", "ISBN", "Author", "Publisher", "Language", "Price"};
         final DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
+
+        try {
+
+            dataList = dataRepository.executeQuery(bookFetchQuery, BookModel.class);
+
+            for (var row : dataList) {
+                BookModel data = (BookModel) row;
+                tableModel.addRow(new String[]{
+                        data.getName(),
+                        String.valueOf(data.getIsbn()),
+                        data.getAuthor(),
+                        data.getPublisher(),
+                        data.getLanguage(),
+                        String.valueOf(data.getPrice())
+                });
+            }
+
+        } catch (DataRepositoryException e) {
+            Alert.showError("Database Error:", e.getMessage());
+        }
 
         table = new JTable(tableModel);
         table.setFillsViewportHeight(true);
