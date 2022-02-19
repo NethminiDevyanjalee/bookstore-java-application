403d05f0aa1b0f86ec4ba40de5978860065b8414
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2022-02-19 22:57:09 +0530

    Integrate category module (#5)
    * Create Category Interface
    
    * final book author module
    
    * update database repository (#3)
    
    * book publisher model completed
    
    * fix errors
    
    * set book Category module
    
    Co-authored-by: 29Niro <29iro@gmail.com>
    Co-authored-by: NethminiDevyanjalee <73274982+NethminiDevyanjalee@users.noreply.github.com>
    Co-authored-by: N-kapila <nirmalkapilarathne1998@gmail.com>

---
+++ src/main/java/com/debuggers/bookstore/models/CategoryModel.java
@@ -0,0 +1,84 @@
+package com.debuggers.bookstore.models;
+
+import com.debuggers.bookstore.repository.ColumnValueMap;
+import com.debuggers.bookstore.repository.DataRepositoryException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CategoryModel implements SqlDataModel{
+    private int id;
+    private String Category;
+    private String subCategoriesStr;
+    private List<String> subCategoriesList = new ArrayList<>();
+    private int isDelete;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String category) {
+        Category = category;
+    }
+
+    public String getSubCategoriesStr() {
+        return subCategoriesStr;
+    }
+
+    public void setSubCategoriesStr(String subCategoriesStr) {
+        this.subCategoriesStr = subCategoriesStr;
+    }
+
+    public List<String> getSubCategoriesList() {
+        return subCategoriesList;
+    }
+
+    public void setSubCategoriesList(List<String> subCategoriesList) {
+        this.subCategoriesList = subCategoriesList;
+    }
+
+    public int getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(int isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    @Override
+    public void readSQL(ResultSet resultSet) throws DataRepositoryException {
+        try {
+            this.id = resultSet.getInt("id");
+            this.subCategoriesStr = resultSet.getString("sub_category");
+            this.Category = resultSet.getString("category");
+            this.isDelete = resultSet.getInt("is_delete");
+
+            if (!this.subCategoriesStr.equals("N/A")) {
+                for (String s:this.subCategoriesStr.split(",")) {
+                    this.subCategoriesList.add(s);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new DataRepositoryException(e, e.getMessage());
+        }
+    }
+
+    @Override
+    public ColumnValueMap writeSQL(ColumnValueMap columnValueMap) throws DataRepositoryException {
+        columnValueMap.set("category",this.Category);
+        columnValueMap.set("is_delete",this.isDelete);
+        return columnValueMap;
+    }
+}
--- src/main/java/com/debuggers/bookstore/views/book/Category.java
+++ src/main/java/com/debuggers/bookstore/views/book/Category.java
@@ -1,13 +1,47 @@
 package com.debuggers.bookstore.views.book;
 
+import com.debuggers.bookstore.models.BookAuthorModel;
+import com.debuggers.bookstore.models.CategoryModel;
+import com.debuggers.bookstore.models.SqlDataModel;
 import com.debuggers.bookstore.repository.DataRepository;
+import com.debuggers.bookstore.repository.DataRepositoryException;
+import com.debuggers.bookstore.views.Alert;
 import com.debuggers.bookstore.views.PageView;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Category extends PageView {
-    private DataRepository dataRepository;
     private JPanel mainPanel;
+    private JTextField txtCategory;
+    private JTextField txtSubCategory;
+    private JScrollPane jScrollPane;
+    private JButton btnAdd;
+    private JButton btnClear;
+    private JButton btnSave;
+    private JButton btnDelete;
+    private JButton btnEdit;
+    private JList subCatList;
+    private JTable table;
+
+    private DataRepository dataRepository;
+    private List<SqlDataModel> dataList;
+    private CategoryModel categoryModel;
+    private String fetchQuery = """
+            SELECT category,book_category.id,book_category.is_delete,
+            IFNULL(GROUP_CONCAT(sub_category),'N/A') AS 'sub_category'
+            FROM book_category
+            LEFT JOIN book_sub_category
+            ON book_sub_category.category_id = book_category.id AND book_sub_category.is_delete = 0
+            WHERE book_category.is_delete = 0
+            GROUP BY book_category.id
+            """;
+    private String subCatInsertQuery = "INSERT INTO book_sub_category(category_id,sub_category) VALUES(%o,'%s')";
 
     public Category(DataRepository dataRepository) {
         super();
@@ -17,5 +51,191 @@ public class Category extends PageView {
 
     private void initComponents() {
         add(mainPanel);
+
+        btnSave.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                insert();
+            }
+        });
+        btnEdit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                edit();
+            }
+        });
+        btnDelete.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                delete();
+            }
+        });
+        btnClear.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                clearFields();
+            }
+        });
+        btnAdd.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                addSubCategory();
+            }
+        });
+
+        createTable();
     }
+
+    private void insert() {
+        if (categoryModel == null)
+            categoryModel = new CategoryModel();
+
+        categoryModel.setCategory(txtCategory.getText());
+
+        if (categoryModel.getCategory().isEmpty()) {
+            Alert.showError("Validation Error:", "Category is Empty!");
+            return;
+        }
+
+        try {
+            int insertID;
+            dataRepository.createStatement("book_category");
+            if (categoryModel.getId() == 0) {
+                insertID = (int) dataRepository.insert(categoryModel);
+            } else {
+                insertID = categoryModel.getId();
+                dataRepository.where("id", categoryModel.getId());
+                dataRepository.update(categoryModel);
+            }
+
+            insertSubCat(insertID);
+
+            clearFields();
+            createTable();
+            categoryModel = null;
+            Alert.showSuccess("Success", "This change has been save!");
+
+        } catch (DataRepositoryException exception) {
+
+            Alert.showError("Database error:", exception.getMessage());
+
+        }
+
+    }
+
+    private void insertSubCat(int insertID) {
+        if (subCatList.getModel().getSize() == 0) return;
+        dataRepository.createStatement("book_sub_category");
+        dataRepository.where("category_id", insertID);
+
+        try {
+
+            dataRepository.delete();
+
+            for (int i = 0; i < subCatList.getModel().getSize(); i++) {
+                dataRepository.execute(subCatInsertQuery.formatted(insertID, subCatList.getModel().getElementAt(i)));
+            }
+
+        } catch (DataRepositoryException exception) {
+            Alert.showError("Database error:", exception.getMessage());
+        }
+    }
+
+    private void edit() {
+        final int selectedRow = table.getSelectedRow();
+
+        if (selectedRow == -1) {
+            Alert.showError("Edit Error:", "Please the record that you want to edit!");
+            return;
+        }
+        categoryModel = (CategoryModel) dataList.get(selectedRow);
+        txtCategory.setText(categoryModel.getCategory());
+        subCatList.setListData(categoryModel.getSubCategoriesList().toArray());
+    }
+
+    private void delete() {
+        final int selectedRow = table.getSelectedRow();
+
+        if (selectedRow == -1) {
+            Alert.showError("Deletion Error:", "Please the record that you want to delete!");
+            return;
+        }
+
+        final int response = Alert.showConfirm("Delete record", "Are you sure you want to delete");
+        categoryModel = (CategoryModel) dataList.get(selectedRow);
+        categoryModel.setIsDelete(1);
+
+        if (response == 0) {
+
+            try {
+
+                dataRepository.createStatement("book_category");
+                dataRepository.where("id", categoryModel.getId());
+                dataRepository.update(categoryModel);
+                categoryModel = null;
+                createTable();
+
+            } catch (DataRepositoryException exception) {
+
+                Alert.showError("Deletion Error:", exception.getMessage());
+
+            }
+        }
+
+    }
+
+    private void clearFields() {
+        Arrays.stream(mainPanel.getComponents()).forEach((c) -> {
+            if (c instanceof JTextField) {
+                ((JTextField) c).setText(null);
+            }
+        });
+        subCatList.setListData(new Object[] {});
+        categoryModel = null;
+    }
+
+    private void addSubCategory() {
+        List<Object> data = new ArrayList<>();
+        String subCat = txtSubCategory.getText();
+
+        if (subCat.isEmpty()) {
+            Alert.showError("Validation Error:", "Sub Category name is required.");
+            return;
+        }
+
+        for (int i = 0; i < subCatList.getModel().getSize(); i++) {
+            data.add(subCatList.getModel().getElementAt(i));
+        }
+
+        data.add(subCat);
+        subCatList.setListData(data.toArray());
+        txtSubCategory.setText(null);
+    }
+
+    private void createTable() {
+        final String[] columnNames = {"Category", "Sub Categories"};
+        final DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
+
+        try {
+
+            dataList = dataRepository.executeQuery(fetchQuery, CategoryModel.class);
+
+            for (var row : dataList) {
+                CategoryModel data = (CategoryModel) row;
+                tableModel.addRow(new String[]{data.getCategory(), data.getSubCategoriesStr()});
+            }
+        } catch (DataRepositoryException e) {
+            e.printStackTrace();
+        }
+
+        table = new JTable(tableModel);
+        table.setFillsViewportHeight(true);
+        jScrollPane.setViewportView(table);
+    }
+
 }
