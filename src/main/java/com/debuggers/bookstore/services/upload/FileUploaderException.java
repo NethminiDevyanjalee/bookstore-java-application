69690676b81a6692545d83ae090fcb006e8b79b4
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2022-02-20 22:44:30 +0530

    File upload integrate (#12)
    * init packages
    
    * complete image upload part

---
+++ src/main/java/com/debuggers/bookstore/services/upload/FileUploaderException.java
@@ -0,0 +1,7 @@
+package com.debuggers.bookstore.services.upload;
+
+public class FileUploaderException extends Throwable {
+    public FileUploaderException(Exception e) {
+        super(e);
+    }
+}
--- src/main/java/com/debuggers/bookstore/views/book/Book.java
+++ src/main/java/com/debuggers/bookstore/views/book/Book.java
@@ -1,17 +1,28 @@
 package com.debuggers.bookstore.views.book;
 
+import com.cloudinary.Url;
 import com.debuggers.bookstore.models.*;
 import com.debuggers.bookstore.repository.DataRepository;
 import com.debuggers.bookstore.repository.DataRepositoryException;
+import com.debuggers.bookstore.services.upload.FileUploader;
+import com.debuggers.bookstore.services.upload.FileUploaderException;
+import com.debuggers.bookstore.services.upload.cloudinary.CloudinaryFileUploader;
 import com.debuggers.bookstore.views.Alert;
 import com.debuggers.bookstore.views.PageView;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -36,6 +47,7 @@ public class Book extends PageView {
     private JComboBox comboCategory;
     private JComboBox comboSubCategory;
     private JTextArea textareaDescription;
+    private JLabel imgViwer;
 
     private List<SqlDataModel> dataList;
     private List<SqlDataModel> authorList;
@@ -43,6 +55,9 @@ public class Book extends PageView {
     private List<SqlDataModel> languageList;
     private List<SqlDataModel> categoryList;
     private BookModel bookModel;
+    private File selectedFile;
+
+    private FileUploader fileUploader;
 
     private String fetchCategoriesQuery = """
             SELECT category,book_category.id,book_category.is_delete,
@@ -56,7 +71,7 @@ public class Book extends PageView {
             """;
     private String bookFetchQuery = """
             SELECT
-            book.id,book.name AS NAME,price,isbn,category_id,sub_category_id,language_id,author_id,publisher_id,description,
+            book.id,book.name AS NAME,price,isbn,category_id,sub_category_id,language_id,author_id,publisher_id,description,img_url,
             CONCAT(book_author.fname,' ',book_author.lname) AS author,
             book_publisher.name AS publisher,
             book_language.language
@@ -71,6 +86,7 @@ public class Book extends PageView {
     public Book(DataRepository dataRepository) {
         super();
         this.dataRepository = dataRepository;
+        this.fileUploader = new CloudinaryFileUploader();
         initComponents();
     }
 
@@ -81,6 +97,7 @@ public class Book extends PageView {
         loadPublisher();
         loadCategory();
         createTable();
+        initDefaultImageView();
 
         btnClear.addMouseListener(new MouseAdapter() {
             @Override
@@ -116,6 +133,50 @@ public class Book extends PageView {
                 loadSubCategory();
             }
         });
+        imgViwer.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                popUpImageSelector();
+            }
+        });
+
+    }
+
+    private void initDefaultImageView() {
+        setImageViewImage(new File("src/main/resources/images/upload.png"));
+    }
+
+    private void setImageViewImage(File file) {
+        try {
+            imgViwer.setIcon(
+                    new ImageIcon(
+                            getScaledImage(
+                                    ImageIO.read(file),
+                                    100,
+                                    100
+                            )
+                    )
+            );
+        } catch (IOException e) {
+            Alert.showError("System Error:", e.getMessage());
+        }
+    }
+
+    private void setImageViewImage(String url) {
+        try {
+            imgViwer.setIcon(
+                    new ImageIcon(
+                            getScaledImage(
+                                    ImageIO.read(new URL(url)),
+                                    100,
+                                    100
+                            )
+                    )
+            );
+        } catch (IOException e) {
+            Alert.showError("System Error:", e.getMessage());
+        }
     }
 
     private void loadLanguage() {
@@ -215,6 +276,14 @@ public class Book extends PageView {
 
     }
 
+    void popUpImageSelector() {
+        JFileChooser fileChooser = new JFileChooser();
+        if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+            selectedFile = fileChooser.getSelectedFile();
+            setImageViewImage(selectedFile);
+        }
+    }
+
     private void insert() {
 
         if (bookModel == null)
@@ -279,7 +348,6 @@ public class Book extends PageView {
         dataRepository.where("isbn", bookModel.getIsbn());
         try {
 
-
             dataRepository.createStatement("book");
             ResultSet resultSet = dataRepository.executeQuery(findISBNExist.formatted(bookModel.getIsbn()));
             if (bookModel.getId() == 0) {
@@ -287,11 +355,17 @@ public class Book extends PageView {
                     Alert.showError("Validation Error:", "This ISBN is already exist!");
                     return;
                 }
+                if (selectedFile != null) {
+                    bookModel.setImageURL(fileUploader.upload(selectedFile));
+                }
                 dataRepository.insert(bookModel);
             } else {
                 if (resultSet.next() && resultSet.getInt("id") != bookModel.getId()) {
                     Alert.showError("Validation Error:", "This ISBN is already exist!");
                     return;
+                }
+                if (selectedFile != null) {
+                    bookModel.setImageURL(fileUploader.upload(selectedFile));
                 }
                 dataRepository.where("id", bookModel.getId());
                 dataRepository.update(bookModel);
@@ -301,7 +375,7 @@ public class Book extends PageView {
             clearFields();
             bookModel = null;
 
-        } catch (DataRepositoryException | SQLException exception) {
+        } catch (DataRepositoryException | SQLException | FileUploaderException exception) {
             Alert.showError("Database Error:", exception.getMessage());
         }
 
@@ -322,7 +396,7 @@ public class Book extends PageView {
         txtISBN.setText(bookModel.getIsbn());
         txtPrice.setText(String.valueOf(bookModel.getPrice()));
         textareaDescription.setText(bookModel.getDescription());
-        System.out.println(bookModel.getAuthorId());
+        setImageViewImage(bookModel.getImageURL());
         BookAuthorModel authorModel = (BookAuthorModel) authorList.stream().filter((e) -> ((BookAuthorModel) e).getId() == bookModel.getAuthorId()).findFirst().get();
         BookPublisherModel publisherModel = (BookPublisherModel) publisherList.stream().filter((e) -> ((BookPublisherModel) e).getId() == bookModel.getPublisherId()).findFirst().get();
         BookLanguageModel languageModel = (BookLanguageModel) languageList.stream().filter((e) -> ((BookLanguageModel) e).getId() == bookModel.getLanguageId()).findFirst().get();
@@ -384,7 +458,9 @@ public class Book extends PageView {
                 ((JComboBox) c).setSelectedIndex(0);
             }
         });
+        initDefaultImageView();
         bookModel = null;
+        selectedFile = null;
     }
 
     private void createTable() {
@@ -414,6 +490,17 @@ public class Book extends PageView {
         table = new JTable(tableModel);
         table.setFillsViewportHeight(true);
         jScrollPane.setViewportView(table);
+    }
+
+    private Image getScaledImage(Image srcImg, int w, int h) {
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImg;
     }
 
 }
